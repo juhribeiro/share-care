e43636f31ce950ae90e4e390065059e91d4b47a2
juliane.ribeiro <juliane.ribeiro@segfy.com>
2020-09-02 07:39:09 -0300

    fix(structure): restructure

---
+++ src/shareCare/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shareCare.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
